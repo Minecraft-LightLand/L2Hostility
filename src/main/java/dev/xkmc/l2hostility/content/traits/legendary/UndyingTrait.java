65ef169b6d3a3965602a3a9289c2d93c79ffd70f
lcy0x1 <arthurlcy0x1@gmail.com>
2023-09-04 18:29:09 +0800

    v2.4.2

--- src/main/java/dev/xkmc/l2hostility/content/traits/legendary/UndyingTrait.java
+++ src/main/java/dev/xkmc/l2hostility/content/traits/legendary/UndyingTrait.java
@@ -1,11 +1,14 @@
 package dev.xkmc.l2hostility.content.traits.legendary;
 
+import dev.xkmc.l2complements.init.registrate.LCEffects;
 import dev.xkmc.l2hostility.init.L2Hostility;
 import dev.xkmc.l2hostility.init.network.TraitEffectToClient;
 import dev.xkmc.l2hostility.init.network.TraitEffects;
 import net.minecraft.ChatFormatting;
 import net.minecraft.tags.DamageTypeTags;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
 import net.minecraftforge.event.ForgeEventFactory;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 
@@ -23,12 +26,27 @@ public class UndyingTrait extends LegendaryTrait {
 		if (event.getSource().is(DamageTypeTags.BYPASSES_INVULNERABILITY)) {
 			return;
 		}
+		if (!validTarget(entity)) {
+			return;
+		}
 		float health = ForgeEventFactory.onLivingHeal(entity, entity.getMaxHealth());
 		entity.setHealth(health);
 		if (!entity.isDeadOrDying()) {
 			event.setCanceled(true);
 			L2Hostility.HANDLER.toTrackingPlayers(new TraitEffectToClient(entity, this, TraitEffects.UNDYING), entity);
 		}
+	}
+
+	@Override
+	public boolean allow(LivingEntity le, int difficulty, int maxModLv) {
+		return validTarget(le) && super.allow(le, difficulty, maxModLv);
+	}
+
+	public boolean validTarget(LivingEntity le) {
+		if (le instanceof EnderDragon){
+			return false;
+		}
+		return le.canBeAffected(new MobEffectInstance(LCEffects.CURSE.get(), 100));
 	}
 
 }
