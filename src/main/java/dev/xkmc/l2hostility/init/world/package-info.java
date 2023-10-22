8623ba9e6563c2ee8e9763c940d0b13fc00b2d79
lcy0x1 <arthurlcy0x1@gmail.com>
2023-10-22 22:08:01 +0800

    init

---
+++ src/main/java/dev/xkmc/l2hostility/init/world/package-info.java
@@ -0,0 +1,8 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+
+package dev.xkmc.l2hostility.init.world;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
