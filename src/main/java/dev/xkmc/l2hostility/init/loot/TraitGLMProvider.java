6a93ad8cac67f18777585d6d239eee853f1abe4b
lcy0x1 <arthurlcy0x1@gmail.com>
2023-10-22 12:27:34 +0800

    tweak grenade drops

--- src/main/java/dev/xkmc/l2hostility/init/loot/TraitGLMProvider.java
+++ src/main/java/dev/xkmc/l2hostility/init/loot/TraitGLMProvider.java
@@ -103,7 +103,9 @@ public class TraitGLMProvider extends GlobalLootModifierProvider {
 		add(LHTraits.AURA.get(), new ItemStack(LCItems.EMERALD, 1), 1, 0, 0.02);
 		add(LHTraits.RAGNAROK.get(), new ItemStack(LCItems.BLACKSTONE_CORE, 1), 1, 0, 0.05);
 		add(LHTraits.SHULKER.get(), new ItemStack(LCItems.CAPTURED_BULLET, 1), 1, 0, 0.2);
-		add(LHTraits.GRENADE.get(), new ItemStack(LCItems.STORM_CORE, 1), 1, 0.1, 0.05);
+		add(LHTraits.GRENADE.get(), new ItemStack(LCItems.STORM_CORE, 1), 3, 0, 0.1);
+		add(LHTraits.GRENADE.get(), new ItemStack(Items.GUNPOWDER, 4), 1, 0, 0.1);
+		add(LHTraits.GRENADE.get(), new ItemStack(Items.CREEPER_HEAD, 1), 5, 0.25, 0);
 
 		add(LHTraits.DISPELL.get(), new ItemStack(LHItems.IMAGINE_BREAKER.get()), 3, 1, 0);
 
